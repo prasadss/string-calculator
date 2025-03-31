815b471185838220ca3f3830d469228d10b3fef2
prasadss <prasadssaitwal@gmail.com>
2025-03-31 17:33:34 +0530

    Supports addition of two numbers

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -4,6 +4,10 @@ test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
 
-test('returns the number itself if only one number is provided', () => {
-    expect(add("1")).toBe(1);
+test("returns the number itself if only one number is provided", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("returns sum of two numbers", () => {
+  expect(add("1,2")).toBe(3);
 });
